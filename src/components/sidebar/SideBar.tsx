a0738334b82709a16b4c4c545bf88afec12e485b
Silvio-Hub <silvioand1@hotmail.com>
2021-03-17 17:12:53 -0300

    Desafio componentização finalizado

---
+++ src/components/sidebar/SideBar.tsx
@@ -0,0 +1,35 @@
+import { Button } from '../Button';
+
+import '../sidebar/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: {
+  genres: Array<GenreResponseProps>,
+  handleClickButton: Function,
+  selectedGenreId: number,
+}) {
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+            key={genre.name}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
+}
